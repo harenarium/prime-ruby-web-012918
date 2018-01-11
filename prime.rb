05661fbed04ac418ff40ff18b15173c1cba3914e
Caroline Lee <carolinel2610@gmail.com>
2018-01-11 22:02:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,16 @@
 # Add  code here!
 def prime? (int)
-  int == 2 || int == 3 || int == 5 || (int.is_a? Integer && int%2 + int%3 +int%5 + int%7 ==0)
+  if (int.is_a? Integer && int >2 && int.odd?)
+    array=(1...int).to_a
+    array.each do |a|
+      if (int%a ==0)
+        return false
+      else
+        new_last = int/a
+        array = (a...new_last).to_a
+      end
+  elsif int == 2 || int == 3 || int == 5 
+    true
+  else
+    false
 end
